aeb7e20552f1712be6518dcb300cd224d0bb2160
Haiden-svg <101202674+Haiden-svg@users.noreply.github.com>
2025-04-05 17:29:04 +0200

    Ajout dossier

---
+++ Jeu_dame/Main.py
@@ -0,0 +1,8 @@
+from Jeu import Jeu
+
+def main():
+    jeu = Jeu()
+    jeu.run()
+
+if __name__ == "__main__":
+    main()
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Jeu_dame : Jeu dame 
+
+Mcts :L'algorithme MCTS a implementer sur le jeu de dame
