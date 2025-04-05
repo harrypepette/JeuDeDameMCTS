25bb7cbf955a899884389e76f3420ee9ddc90bd6
Haiden-svg <101202674+Haiden-svg@users.noreply.github.com>
2025-04-05 17:21:54 +0200

    Rangement dans leurs dossier respective

--- Joueur.py
+++
@@ -1,7 +0,0 @@
-class Joueur:
-    def __init__(self, couleur):
-        """
-        Initialise un joueur.
-        :param couleur: La couleur des pions du joueur ('blanc' ou 'noir').
-        """
-        self.couleur = couleur
--- Main.py
+++
@@ -1,8 +0,0 @@
-from Jeu import Jeu
-
-def main():
-    jeu = Jeu()
-    jeu.run()
-
-if __name__ == "__main__":
-    main()
