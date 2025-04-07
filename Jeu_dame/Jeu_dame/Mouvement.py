1a70c4662f8c1ef28abed7428db2f10fe6f088c6
Haiden-svg <101202674+Haiden-svg@users.noreply.github.com>
2025-04-07 13:07:04 +0200

    supression de l'ancien projet Jeu_dame en double

--- Jeu_dame/Jeu_dame/Mouvement.py
+++
@@ -1,9 +0,0 @@
-class Mouvement:
-    def __init__(self, depart, arrivee):
-        """
-        Initialise un mouvement.
-        :param depart: Position de départ (tuple (x, y)).
-        :param arrivee: Position d'arrivée (tuple (x, y)).
-        """
-        self.depart = depart
-        self.arrivee = arrivee
